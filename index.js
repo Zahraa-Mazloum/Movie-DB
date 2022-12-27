739f057f0e9bfd66bdf085ea087405c6c3a8656d
zahraa mazloum <zahraamazloum2001@gmail.com>
2022-12-27 10:57:49 +0200

    Step 4: Let's complicate the API

--- index.js
+++ index.js
@@ -6,15 +6,6 @@ app.get('/', (req, res) => {
     res.send('Ok');
 });
 
-app.listen(port, (error) => {
-    if (!error) {
-        console.log('Ok');
-    }
-    else {
-        console.log('Something went wrong');
-    }
-});
-
 app.get('/test', (req, res) => {
     res.json({ status: 200, message: 'ok' });
 });
@@ -24,4 +15,28 @@ app.get('/time', (req, res) => {
     const minutes = currentTime.getMinutes();
     res.json({ status: 200, message: `${hours}:${minutes}` });
 
+});
+
+app.get('/hello/:id', (req, res) => {
+    const id = req.params.id;
+    res.json({ status: 200, message: `Hello, ${id}` });
+});
+
+app.get('/search', (req, res) => {
+    const search = req.query.s
+        ;
+    if (search) {
+        res.json({ status: 200, message: 'ok', data: search });
+    } else {
+        res.status(500).json({ status: 500, error: true, message: 'you have to provide a search' });
+    }
+});
+
+app.listen(port, (error) => {
+    if (!error) {
+        console.log('Ok');
+    }
+    else {
+        console.log('Something went wrong');
+    }
 });
